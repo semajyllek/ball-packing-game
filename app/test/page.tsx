6411edaa32d86cf25e082452b45fea3285a5a525
James Kelly <jkelly@sensoryinc.com>
2024-11-17 10:59:23 -0800

    testing

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,19 @@
+// app/test/page.tsx
 'use client';
-
-const TEST_OUTLINE_URL = 'https://flower-filler-bucket.s3.us-west-2.amazonaws.com/flower_dataset/processed_outlines/flower_00000001.json';
 
 export default function TestPage() {
     return (
         <div>
-            <div>Testing URL:</div>
-            <a href={TEST_OUTLINE_URL} target="_blank" rel="noopener noreferrer">
-                Click to test URL directly
-            </a>
+            <div>Testing direct fetch:</div>
+            <button 
+                onClick={async () => {
+                    const response = await fetch('https://flower-filler-bucket.s3.us-west-2.amazonaws.com/flower_dataset/processed_outlines/flower_00000001.json');
+                    const data = await response.json();
+                    console.log('Data:', data);
+                }}
+            >
+                Fetch Data
+            </button>
         </div>
     );
 }
